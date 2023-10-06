451e03248a4e81cbbf35c782c0178898169666e8
Elena <femtochka@yahoo.com>
2023-10-06 16:42:19 +0300

    start

---
+++ WebApplication10/Models/Requests/UpdateClientRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApplication10.Models.Requests
+{
+    public class UpdateClientRequest
+    {
+    }
+}
---
+++ WebApplication10/Program.cs
@@ -0,0 +1,81 @@
+
+using Microsoft.Data.Sqlite;
+
+namespace WebApplication10
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ConfigureSqlLiteConnection();
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+
+        private static void ConfigureSqlLiteConnection()
+        {
+            const string connectionString = "Data Source = clinic.db;";
+            var connection = new SqliteConnection (connectionString);
+            connection.Open ();
+            PrepareSchema(connection);
+        }
+        private static void PrepareSchema(SqliteConnection connection)
+        {
+            SqliteCommand sqliteCommand = connection.CreateCommand ();
+            sqliteCommand.CommandText = "DROP TABLE IF EXISTS consultations";
+            sqliteCommand.ExecuteNonQuery ();
+            sqliteCommand.CommandText = "DROP TABLE IF EXISTS pets";
+            sqliteCommand.ExecuteNonQuery();
+            sqliteCommand.CommandText = "DROP TABLE IF EXISTS clients";
+            sqliteCommand.ExecuteNonQuery();
+
+            sqliteCommand.CommandText =
+                @"CREATE TABLE Clients(
+                ClientId INTEGER PRIMARY KEY, 
+                Document TEXT, 
+                SurName TEXT, 
+                FirstName TEXT, 
+                Patronymic TEXT, 
+                Birthday INTEGER)";
+            sqliteCommand.ExecuteNonQuery();
+            sqliteCommand.CommandText =
+               @"CREATE TABLE Pets(
+                PetId INTEGER PRIMARY KEY, 
+                ClientId INTEGER, 
+                Name TEXT, 
+                Birthday INTEGER)";
+            sqliteCommand.ExecuteNonQuery();
+            sqliteCommand.CommandText =
+               @"CREATE TABLE Consultations(
+                ConsultationId INTEGER PRIMARY KEY, 
+                ClientId INTEGER,
+                PetId INTEGER 
+                ConsultationDate INTEGER,
+                Description TEXT)";
+            sqliteCommand.ExecuteNonQuery();
+        }
+    }
+    
+}
